f7e2699761f937193f731582f70ba72db7fa1313
kazazian <arina.kazazian07@mail.ru>
2026-03-15 01:59:21 +0400

    2

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+    let lights = document.querySelectorAll('[data-js="lightOn"]');
+    let index = 0;
+    lights.forEach((light) => {
+        let broken = false;
+        light.addEventListener('click', () => {
+            const options = ['images/LightOff.png', 'images/LightOn.png'];
+            light.src = broken ? options[1] : options[0];
+            if (!broken) {
+                let Sound = new Audio("glass.mp3")
+                Sound.play()
+            }
+            broken = !broken
+        });
+    })
+});
